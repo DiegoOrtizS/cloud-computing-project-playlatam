1d77ec3892194347fd09477a3aa02a0341d116fa
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-06-23 23:59:34 -0500

    refactor: solve pylint

--- src/handlers/main.py
+++ src/handlers/main.py
@@ -1,6 +1,6 @@
 from scraping.scraper import Scraper
 
 
-def handler(event, context) -> dict:
+def handler(event, _context) -> dict:
     Scraper(event["tournament_id"]).main()
     return {"statusCode": 200, "body": "OK"}
